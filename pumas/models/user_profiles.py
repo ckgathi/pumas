5ac747a730f9613812a580fa2365109f948e2993
Coulson <ckgathi@bhp.org.bw>
2016-11-13 16:29:54 +0200

    link student to lecture

--- pumas/models/user_profiles.py
+++ pumas/models/user_profiles.py
@@ -2,29 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Student(models.Model):
-
-    user = models.OneToOneField(User)
-
-    student_id = models.IntegerField(
-        verbose_name='Student Identifier',
-        unique=True,
-        help_text="Student identifier",
-        editable=False,
-        db_index=True)
-
-    faculty = models.CharField(
-        verbose_name="Faculty",
-        max_length=150)
-
-    programe = models.CharField(
-        verbose_name="Programe of Study",
-        max_length=150)
-
-    class Meta:
-        app_label = 'pumas'
-
-
 class Lecture(models.Model):
 
     user = models.OneToOneField(User)
@@ -38,6 +15,31 @@ class Lecture(models.Model):
     is_supervisor = models.NullBooleanField(
         verbose_name='Are you a supervisor',
         default=False,)
+
+    class Meta:
+        app_label = 'pumas'
+
+
+class Student(models.Model):
+
+    user = models.OneToOneField(User)
+
+    student_id = models.IntegerField(
+        verbose_name='Student Identifier',
+        unique=True,
+        help_text="Student identifier",
+        editable=False,
+        db_index=True)
+
+    supervisor = models.OneToOneField(Lecture)
+
+    faculty = models.CharField(
+        verbose_name="Faculty",
+        max_length=150)
+
+    programe = models.CharField(
+        verbose_name="Programe of Study",
+        max_length=150)
 
     class Meta:
         app_label = 'pumas'
